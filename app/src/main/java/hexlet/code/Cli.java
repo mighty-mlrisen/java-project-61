f6c1865a0e3fef6cd3f1e8c0906c2e49f15c8b23
Artyom Mazurenko <artemmazurenko71@gmail.com>
2023-04-07 22:59:12 +0300

    testing checkstyle

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,7 +3,7 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void inputName(){
+    public static void inputName() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
