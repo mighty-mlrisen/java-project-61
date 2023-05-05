29577c959b2f868fe809d144a0e41d2feb9bb307
Artyom Mazurenko <artemmazurenko71@gmail.com>
2023-05-05 20:45:02 +0300

    feat: adding game selection and launch functions

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,23 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 class App {
     public static void main(String[] args) {
-        Cli.inputName();
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet\n" + "2 - Even\n" + "0 - Exit");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Your choice: ");
+        int number = scanner.nextInt();
+        switch (number) {
+            case 0:
+                break;
+            case 1:
+                Cli.greeting();
+                break;
+            case 2:
+                Even.startingGame();
+                break;
+        }
     }
 }
