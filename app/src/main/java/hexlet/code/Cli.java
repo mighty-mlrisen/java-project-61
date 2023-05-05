65897c38d8a2d5f40d849f622dc813a9c84ee0de
Artyom Mazurenko <artemmazurenko71@gmail.com>
2023-05-05 20:41:36 +0300

    feat: method greeting returns the user name

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,11 +3,13 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void inputName() {
+    public static String greeting() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println();
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         String name = scanner.next();
         System.out.println("Hello, " + name + "!");
+        return name;
     }
 }
