9391def86a1fdb7c9203baca75e702533c44cfb6
Artyom Mazurenko <artemmazurenko71@gmail.com>
2023-05-09 12:13:36 +0300

    feat: added interaction with the Engine.java file

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,13 +1,19 @@
 package hexlet.code;
 
+import hexlet.code.games.Even;
+import hexlet.code.games.Calc;
 import java.util.Scanner;
 
-class App {
+public class App {
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet\n" + "2 - Even\n" + "0 - Exit");
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("1 - Greet\n" + "2 - Even\n" + "3 - Calc\n" + "0 - Exit");
         System.out.print("Your choice: ");
+        Engine.generalStructure();
+    }
+
+    public static int gameSelection() {
+        Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
         switch (number) {
             case 0:
@@ -18,6 +24,10 @@ class App {
             case 2:
                 Even.startingGame();
                 break;
+            case 3:
+                Calc.startingGame();
+                break;
         }
+        return number;
     }
 }
