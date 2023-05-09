c6d95bcca3fc611dbb7b416755200626e802c95d
Artyom Mazurenko <artemmazurenko71@gmail.com>
2023-05-09 12:20:06 +0300

    feat: interaction of the game with the general architecture Engine.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,37 +1,24 @@
-package hexlet.code;
+package hexlet.code.games;
 
+import hexlet.code.Cli;
+import hexlet.code.Engine;
 import org.apache.commons.lang3.RandomUtils;
 import java.util.Scanner;
 
 public class Even {
-    public static String even(int number) {
-        if (number % 2 == 0) return "yes";
-        else return "no";
+    public static void startingGame() {
+        Engine.launchGreeting();
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
     }
 
-    public static void startingGame() {
-        String name = Cli.greeting();
-        int countCorrectAnswer = 0;
-        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-        for (int i = 0; i < 3; i++) {
-            int number = RandomUtils.nextInt(0, 1000);
-            String correctAnswer = Even.even(number);
-            System.out.print("Question: " + number + "\n" + "Your answer: ");
-            Scanner scanner = new Scanner(System.in);
-            String userAnswer = scanner.next();
-            if (correctAnswer.equals(userAnswer)) {
-                System.out.println("Correct!");
-                countCorrectAnswer += 1;
-                if (countCorrectAnswer == 3) {
-                    System.out.println("Congratulations, " + name + "!");
-                }
-            }
-            else {
-                System.out.println("'" + userAnswer + "' " + "is wrong answer ;(. Correct answer was " + "'" + correctAnswer + "'" + ".");
-                System.out.println("Let's try again, " + name + "!");
-                break;
-            }
-        }
+    public static String generatingQuestion() {
+        int number = RandomUtils.nextInt(0,1000);
+        return Integer.toString(number);
+    }
 
+    public static String correctAnswer(String question) {
+        int number = Integer.parseInt(question);
+        if (number % 2 == 0) return "yes";
+        else return "no";
     }
 }
