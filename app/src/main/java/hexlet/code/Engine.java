8f568b31d56d35840b3196dc8d4dc174db04eaad
Artyom Mazurenko <artemmazurenko71@gmail.com>
2023-05-21 11:10:46 +0300

    feat: after adding games, there is no need to make changes to the engine

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -8,41 +8,20 @@ import hexlet.code.games.Calc;
 import java.util.Scanner;
 
 public class Engine {
-    private static final int GAME_NUMBER_EVEN = 2;
-    private static final int GAME_NUMBER_CALC = 3;
-    private static final int GAME_NUMBER_GCD = 4;
-    private static final int GAME_NUMBER_PROGRESSION = 5;
 
-    public static void playGame() {
-        int gameNumber = App.gameSelection();
-        if (gameNumber == 0 || gameNumber == 1) {
-            return;
-        } else {
-            launchGeneralStructure(gameNumber);
-        }
-    }
-    private static void launchGeneralStructure(int gameNumber) {
-        int countCorrectAnswer = 0;
-        final int numberOfQuestions = 3;
-        for (int i = 0; i < numberOfQuestions; i++) {
-            String correctAnswer = Engine.getCorrectAnswer(gameNumber);
-            String userAnswer = Engine.getUserAnswer();
-            if (correctAnswer.equals(userAnswer)) {
-                System.out.println("Correct!");
-                countCorrectAnswer += 1;
-                Engine.checkNumberOfCorrectAnswers(countCorrectAnswer);
-            } else {
-                Engine.getWrongAnswer(userAnswer, correctAnswer);
-                break;
-            }
-        }
-    }
-
-    private static String getCorrectAnswer(int gameNumber) {
-        String question = Engine.questionSelection(gameNumber, 1, null);
+    public static boolean launchGeneralStructure(String [] values, int index) {
+        String question = values[0];
+        String correctAnswer = values[1];
         System.out.print("Question: " + question + "\n" + "Your answer: ");
-        String correctAnswer = Engine.questionSelection(gameNumber, 2, question);
-        return correctAnswer;
+        String userAnswer = Engine.getUserAnswer();
+        if (userAnswer.equals(correctAnswer)){
+            System.out.println("Correct!");
+            Engine.checkNumberOfCorrectAnswers(index);
+            return true;
+        } else {
+            Engine.getWrongAnswer(userAnswer, correctAnswer);
+            return false;
+        }
     }
 
     private  static void checkNumberOfCorrectAnswers(int countCorrectAnswer) {
@@ -65,56 +44,5 @@ public class Engine {
 
     public static void launchGreeting() {
         Cli.greeting();
-    }
-
-    public static String questionSelection(int gameNumber, int index, String question2) {
-        String question = "";
-        String correctAnswer = "";
-        if (index == 1) {
-            question = Engine.generateQuestionByNumber(gameNumber);
-        } else {
-            correctAnswer = Engine.getCorrectAnswerByNumber(gameNumber, question2);
-        }
-        return getSelectedContent(index, question, correctAnswer);
-    }
-
-    private static String generateQuestionByNumber(int gameNumber) {
-        String question = "";
-        if (gameNumber == GAME_NUMBER_EVEN) {
-            question = Even.generateEvenQuestion();
-        } else if (gameNumber == GAME_NUMBER_CALC) {
-            question = Calc.generateCalcQuestion();
-        } else if (gameNumber == GAME_NUMBER_GCD) {
-            question = GCD.generateGCDQuestion();
-        } else if (gameNumber == GAME_NUMBER_PROGRESSION) {
-            question = Progression.generateProgressionQuestion();
-        } else {
-            question = Prime.generatePrimeQuestion();
-        }
-        return question;
-    }
-
-    private static String getCorrectAnswerByNumber(int gameNumber, String question2) {
-        String correctAnswer = "";
-        if (gameNumber == GAME_NUMBER_EVEN) {
-            correctAnswer = Even.getEvenCorrectAnswer(question2);
-        } else if (gameNumber == GAME_NUMBER_CALC) {
-            correctAnswer = Calc.getCalcCorrectAnswer(question2);
-        } else if (gameNumber == GAME_NUMBER_GCD) {
-            correctAnswer = GCD.getGCDCorrectAnswer(question2);
-        } else if (gameNumber == GAME_NUMBER_PROGRESSION) {
-            correctAnswer = Progression.getProgressionCorrectAnswer(question2);
-        } else {
-            correctAnswer = Prime.getPrimeCorrectAnswer(question2);
-        }
-        return correctAnswer;
-    }
-
-    private static String getSelectedContent(int index, String question, String correctAnswer) {
-        if (index == 1) {
-            return question;
-        } else {
-            return correctAnswer;
-        }
     }
 }
